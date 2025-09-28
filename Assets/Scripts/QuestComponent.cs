cd69cebe6bbe748c5c74d558af9736359f260385
clorbiecke <144703042+clorbiecke@users.noreply.github.com>
2025-09-28 12:29:08 -0500

    Created QuestComponent and QuestData classes

---
+++ Assets/Scripts/QuestComponent.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class QuestComponent : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("QuestData object that describes a quest.")]
+    private QuestData quest = null;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        QuestManager manager = QuestManager.Instance;
+        if (manager != null )
+        {
+            // TODO: add quest data object to manager
+
+            quest.ResetQuest();
+        }
+        else
+        {
+            Debug.LogError("QuestManager.Instance is null!");
+        }
+        
+    }
+
+
+}
---
+++ Assets/Scripts/QuestData.cs
@@ -0,0 +1,131 @@
+using UnityEngine;
+
+[System.Serializable]
+public class QuestData
+{
+    [SerializeField]
+    [Tooltip("Quest ID")]
+    private string id = "not set";
+
+    [SerializeField]
+    [Tooltip("The quest name to display to players.")]
+    private string displayName = "not set";
+
+    [SerializeField]
+    [Tooltip("An array of instructions, one for each step in the quest.")]
+    private string[] stepInstructions = null;
+
+    // Which step of the quest the player is on
+    private int currentStep = -1;
+
+
+    public QuestData(string id,  string displayName, string[] stepInstructions)
+    {
+        if (id == null || id == "")
+        {
+            Debug.LogError("Quest member variable 'id' is empty!");
+        }
+        else
+        {
+            this.id = id;
+        }
+
+        if (displayName == null || displayName == "")
+        {
+            Debug.LogWarning("Quest member variable 'displayName' is empty!");
+        }
+        else
+        {
+            this.displayName = displayName;
+        }
+
+
+        this.stepInstructions = stepInstructions;
+        currentStep = -1;
+    }
+
+    // Getter for id
+    public string GetID()
+    {
+        return id;
+    }
+
+    // Getter for displayName
+    public string GetDisplayName()
+    {
+        return displayName;
+    }
+
+    // Setter for displayName
+    public void SetDisplayName(string newName)
+    {
+        displayName = newName;
+    }
+
+    // Getter for currentStep
+    public int GetCurrentStep()
+    {
+        return currentStep;
+    }
+
+    // Get instructions for current step
+    public string GetCurrentInstructions()
+    {
+        // if quest not started..
+        if (currentStep < 0)
+        {
+            return "--";
+        }
+        // else if quest completed...
+        else if (currentStep >=  stepInstructions.Length)
+        {
+            return "COMPLETED";
+        }
+        // else (quest is in progress)
+        else
+        {
+            return stepInstructions[currentStep];
+        }
+    }
+
+    // Get total number of steps in quest
+    public int GetTotalSteps()
+    {
+        return stepInstructions.Length;
+    }
+
+    // Resets currentStep to -1, marking the quest as not started
+    public void ResetQuest()
+    {
+        currentStep = -1;
+    }
+
+    // Advances the quest to and returns the next step, or, if bool jumpToEnd is true, completes the quest
+    public int NextStep(bool jumpToEnd = false)
+    {
+        currentStep = (jumpToEnd) ? currentStep + 1 : stepInstructions.Length;
+        return currentStep;
+    }
+
+    // Returns true if currentStep is greater than or equal to 0, and false otherwise
+    public bool IsStarted()
+    {
+        return (currentStep >= 0);
+    }
+
+    // Returns true if currentStep is greater than or equal to the total number of steps
+    public bool IsComplete()
+    {
+        return (currentStep >= stepInstructions.Length);
+    }
+
+    // Returns a single line including all member variables
+    override public string ToString()
+    {
+        return $"{id}, {displayName}, {currentStep}, {stepInstructions}";
+    }
+
+
+
+
+}
